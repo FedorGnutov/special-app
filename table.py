b2bd30996fd1bb372b3246adfd8917b4806bbf29
Fedor Gnutov <fedor_gnutov@mail.ru>
2021-11-20 16:12:23 +0300

    'new-cont'

--- table.py
+++ table.py
@@ -2,5 +2,4 @@ for x in range(1, 11):
     for y in range(1, 11):
         print(x * y, end='\t')
     print('')
-x = int(input('ведите что-нибудь '))
-y = input('И ещё что-нибудь')
+x = (input('ведите что-нибудь ')
